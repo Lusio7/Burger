a0f199756ee503ae45f6f0517bab9229630f23c5
LusioF <lusiof@gmail.com>
2020-02-22 17:50:59 -0500

    schema and seeds sql files and database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Slider', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
